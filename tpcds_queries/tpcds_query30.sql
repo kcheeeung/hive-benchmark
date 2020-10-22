825eaacc01fb2b7ecfba8543ab7a9bedf62c8db9
Kevin Cheung <kecheung@microsoft.com>
2020-10-21 19:34:13 -0700

    Revert "Fix query 30"
    This reverts commit a988f0c913ae981918365b617e9f000aecdc2223.

--- tpcds_queries/tpcds_query30.sql
+++ tpcds_queries/tpcds_query30.sql
@@ -1,9 +1,5 @@
 -- start query 30 in stream 0 using template query30.tpl
 with customer_total_return as
-(
-select ctr_customer_sk, ctr_state, ctr_total_return,
-       avg(ctr_total_return) over(partition by ctr_state ) as ctr_state_avg
-from
  (select wr_returning_customer_sk as ctr_customer_sk
         ,ca_state as ctr_state, 
    sum(wr_return_amt) as ctr_total_return
@@ -14,16 +10,16 @@ from
    and d_year =2000
    and wr_returning_addr_sk = ca_address_sk 
  group by wr_returning_customer_sk
-         ,ca_state
-) x
-)
+         ,ca_state)
   select  c_customer_id,c_salutation,c_first_name,c_last_name,c_preferred_cust_flag
        ,c_birth_day,c_birth_month,c_birth_year,c_birth_country,c_login,c_email_address
        ,c_last_review_date_sk,ctr_total_return
  from customer_total_return ctr1
      ,customer_address
      ,customer
- where ctr1.ctr_total_return > ctr1.ctr_state_avg*1.2
+ where ctr1.ctr_total_return > (select avg(ctr_total_return)*1.2
+        from customer_total_return ctr2 
+                      where ctr1.ctr_state = ctr2.ctr_state)
        and ca_address_sk = c_current_addr_sk
        and ca_state = 'GA'
        and ctr1.ctr_customer_sk = c_customer_sk
