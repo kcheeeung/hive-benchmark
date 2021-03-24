c1516b37cf43f28eb4ad437d2e28f3a58e30f991
k.cheung@berkeley.edu <k.cheung@berkeley.edu>
2021-03-24 03:05:40 -0700

    Capture query time from logs

--- parselog.py
+++ parselog.py
@@ -10,6 +10,26 @@ os.environ["TZ"]="US/Pacific"
 time_id = datetime.datetime.now().strftime("%m.%d.%Y-%H.%M")
 OUT_NAME = "llapio_summary" + time_id + ".csv"
 
+def getPreciseTime(path):
+    """
+        Returns the precise time of query by using the recorded in the logs. Also assumes target is formatted as:
+        "1 row selected (1.23 seconds)"
+        "2 rows selected (1.23 seconds)"
+    """
+    found = False
+    total_time = 0
+
+    with open(path, "r") as file:
+        for line in file:
+            if "rows selected (" in line or "row selected (" in line:
+                total_time += float(re.findall("[0-9]+\.[0-9]+", line)[0])
+                found = True
+
+    if found:
+        return total_time
+    else:
+        return 0.01234
+
 def getCacheHitRatio(path):
     """ Returns the cache hit ratio """
     cacheHit, miss, total = 0, 0, 0
@@ -46,7 +66,7 @@ def getMetadataHitRatio(path):
         # query fail
         return 0.12345
 
-def write_csv(cacheHitRatios, metadataHitRatio):
+def write_csv(queryTime, cacheHitRatios, metadataHitRatio):
     """
         Writes info to a csv file.
         Modify by adding new columns and map of parsed data.
@@ -57,13 +77,14 @@ def write_csv(cacheHitRatios, metadataHitRatio):
     with open(OUT_NAME, "w", newline="") as output_csv:
         writer = csv.writer(output_csv)
         # header
-        head = ["Query#", "Cache Hit %", "Metadata Hit %"]
+        head = ["Query#", "Secs", "Cache Hit %", "Metadata Hit %"]
         writer.writerow(head)
         # info
         for i in queryNum:
-            writer.writerow([float(i), cacheHitRatios[i], metadataHitRatio[i]])
+            writer.writerow([float(i), queryTime[i], cacheHitRatios[i], metadataHitRatio[i]])
 
 def main():
+    querynum_to_timetaken = {}
     querynum_to_cacheratio = {}
     querynum_to_metadatahitratio = {}
     for filename in os.listdir(LOG_FOLDER):
@@ -73,12 +94,13 @@ def main():
                 query_num = query_runNum[0]
                 filepath = LOG_FOLDER + filename
 
+                querynum_to_timetaken[query_num] = getPreciseTime(filepath)
                 querynum_to_cacheratio[query_num] = getCacheHitRatio(filepath)
                 querynum_to_metadatahitratio[query_num] = getMetadataHitRatio(filepath)
             else:
                 raise Exception("Did not find query number in " + query_runNum)
 
-    write_csv(querynum_to_cacheratio, querynum_to_metadatahitratio)
+    write_csv(querynum_to_timetaken, querynum_to_cacheratio, querynum_to_metadatahitratio)
 
 if __name__ == "__main__":
     start = time.time()
