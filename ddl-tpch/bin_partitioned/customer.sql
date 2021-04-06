d0601ce7cdb77354a2d5b113c0d385c451fc6b2e
Kevin Cheung <kecheung@microsoft.com>
2021-04-05 18:15:38 -0700

    DDL update

--- ddl-tpch/bin_partitioned/customer.sql
+++ ddl-tpch/bin_partitioned/customer.sql
@@ -2,11 +2,7 @@ create database if not exists ${DB};
 use ${DB};
 
 drop table if exists customer;
-
 create table customer
 stored as ${FILE}
-TBLPROPERTIES('orc.bloom.filter.columns'='*','orc.compress'='ZLIB')
 as select * from ${SOURCE}.customer
-cluster by C_MKTSEGMENT
-;
-
+cluster by C_MKTSEGMENT;
